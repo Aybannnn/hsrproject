990280831095cd0fff04dd1c9fd9017c3fe663e7
Aybannnn <104247365+Aybannnn@users.noreply.github.com>
2024-03-16 22:18:50 +0800

    header information

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@40,100,0,-25" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="{{url('styles/main.css')}}">
     <link rel="stylesheet" href="{{url('styles/landing.css')}}">
     <title>Honkai Star Rail</title>
@@ -41,7 +41,13 @@
         </nav>
     </header>
     <main>
-        <section id="home">
+        <section id="home" style="position: relative;">
+            <div class="spacer">
+                <span class="material-symbols-outlined">
+                    tune
+                </span>
+                <h1>Download Now</h1>
+            </div>
             <div class="video-container">
                 <video autoplay loop muted style="width: 100%;">
                     <source src="{{asset('videos/hsrbackground.mp4')}}" type="video/mp4">
